cefe65077a7e39b9a65f86acbd409893f90faf0d
chrisnguyen12 <viet27839@gmail.com>
2022-11-04 11:23:43 -0500

    updated code for schema.sql

---
+++ sql/schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE if exsits employeesDB;
+CREATE DATABASE employeesDB;
+USE employeesDB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(45) NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT
+    first_name VARCHAR(45) NULL,
+    last_name VARCHAR(45) NULL
+    role_id INT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
+)
